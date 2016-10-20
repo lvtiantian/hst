14d7bcbf6d5f515e79e9a53e30dfd34099988b48
lvtiantian <793865535@qq.com>
2016-10-20 14:10:06 +0800

    xg

--- js/scripts.js
+++ js/scripts.js
@@ -87,9 +87,9 @@ $(function () {
 $('a[data-rel]').each(function () {
     $(this).attr('rel', $(this).attr('data-rel')).removeAttr('data-rel');
 });
-$("a[rel^='prettyPhoto']").prettyPhoto();
-jQuery("a[rel^='prettyPhoto'], a[rel^='lightbox']").prettyPhoto({
-    overlay_gallery: false, social_tools: false, deeplinking: false
-});
+//$("a[rel^='prettyPhoto']").prettyPhoto();
+//jQuery("a[rel^='prettyPhoto'], a[rel^='lightbox']").prettyPhoto({
+//    overlay_gallery: false, social_tools: false, deeplinking: false
+//});
 
 
